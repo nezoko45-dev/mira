dbc534de33f485247acd57068afd0edb5a4f0e97
daki <nezoko45@gmail.com>
2026-09-14 07:36:48 -0500

    Expose packaged Luna image assets to renderer

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,4 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const path = require('path');
+const { pathToFileURL } = require('url');
 contextBridge.exposeInMainWorld('lunaDesktop', {
-  voiceStatus: () => ipcRenderer.invoke('voice-status')
+  voiceStatus: () => ipcRenderer.invoke('voice-status'),
+  assetUrl: (name) => pathToFileURL(path.join(process.resourcesPath, name)).href
 });
