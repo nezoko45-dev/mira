a2550a9ada247a8fe7bdfa9e293458a4b003ee6c
daki <nezoko45@gmail.com>
2026-09-18 19:08:38 -0500

    Add Mira backend stop script

---
+++ stop-mira.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -NoProfile -Command "Get-CimInstance Win32_Process -Filter \"Name='powershell.exe'\" | Where-Object { $_.CommandLine -like '*server.ps1*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }"
