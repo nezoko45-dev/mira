43673718a59800322ebd4afcf53695d673424217
daki <nezoko45@gmail.com>
2026-09-18 19:55:28 -0500

    Rebuild Mira as simple HTML dating app

--- stop-mira.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell.exe -NoProfile -Command "Get-CimInstance Win32_Process -Filter \"Name='powershell.exe'\" | Where-Object { $_.CommandLine -like '*server.ps1*' } | ForEach-Object { Stop-Process -Id $_.ProcessId -Force }"
